5ba6ceefed1f086a27b41f31ee757aafb23e1bab
Ollie <herbertoliver808@gmail.com>
2025-09-26 13:09:09 +0100

    Point binaryTarget to GitHub Release asset URL

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "novin_intelligence",
-            url: "https://raw.githubusercontent.com/Novinintelligence/novin_intelligence/main/artifacts/novin_intelligence.xcframework.zip",
+            url: "https://github.com/Novinintelligence/novin_intelligence/releases/download/1.0.0/novin_intelligence.xcframework.zip",
             checksum: "93158d742b87531b1f77020c6692097b2cc8b09f535e0d41a687d29f517a4f76"
         )
     ]
